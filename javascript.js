46e9381630dba68e10499caac587c0d28b48c27f
Maryam2329 <125149336+Maryam2329@users.noreply.github.com>
2023-05-09 18:38:45 +0200

    Files finished, this version is working and clean

---
+++ javascript.js
@@ -0,0 +1,60 @@
+
+
+let etchASketch = function (gridsize){
+    const container = document.querySelector('body');
+    // start the creation of the grid
+    const content = document.createElement('div');
+    content.style.cssText = 'color: blue; background: white;';
+    content.style.border="thick solid blue";
+    content.style.display='flex';
+    container.appendChild(content);
+    for(i=1;i<gridsize;i++){
+        const content_column = document.createElement('div');
+        content_column.style.cssText = 'color: red;';
+        content_column.style.border="thick solid red";
+        content_column.style.flex='1';
+        content_column.style.display='flex';
+        content_column.style.flexDirection='column';
+        content.appendChild(content_column);
+        for(j=1;j<gridsize;j++){
+            const content_element = document.createElement('div');
+            content_element.textContent = 'Grid element C'+i+'L'+j;
+            content_element.style.cssText = 'color: orange; background: purple;';
+            content_element.style.border="thick solid orange";
+            content_element.style.display='flex';
+            content_element.style.flexDirection='column';
+            content_element.addEventListener("mouseover", () => {
+                content_element.style.cssText = 'color: orange; background: lightpink;';
+                content_element.style.border="thick solid orange";
+            });
+            content_element.addEventListener("mouseout", () => {
+                content_element.style.cssText = 'color: orange; background: purple;';
+                content_element.style.border="thick solid orange";
+            });
+            content_column.appendChild(content_element);
+            
+        }
+    }
+};
+let getGridSize = function (){
+    let n = 17;
+    const container = document.querySelector('body');
+    // create a button to ask for the number of grids
+    const button = document.createElement('button');
+    button.textContent = 'Click me to define the grid size you want.';
+    button.style.border="thick solid darkgrey";
+    button.style.marginBottom='30px';
+    button.addEventListener('click',()=>{
+        let m=prompt('Please enter the grid size you want to display.');
+        if (m>0){
+            n=Math.round(m);
+            etchASketch(n);
+        } else {
+            alert('What you entered is not a positive number, therefore the default value of 16 will be used.');
+            etchASketch(n);
+        }
+    }
+    );
+    container.appendChild(button);
+}
+getGridSize();
